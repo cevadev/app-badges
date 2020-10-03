265846df97844866375904814dc184911fca454c
Carlos Eduardo Villanueva Altuna <carlos.villanueva.altuna@gmail.com>
2020-10-03 20:57:24 -0300

    Curso ReactJS

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@
 
 // container.appendChild(element);
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const element = <h1>Hello, Platzi Badges!</h1>;
+const name = "Carlos Villanueva";
+const jsx = <h1>Hello soy {name}, Platzi Badges from react!</h1>;
+const element = React.createElement("h1", {}, `Hola, me llamo ${name}`);
 
-const container = document.getElementById('app');
+const container = document.getElementById("app");
 
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(element, container);
+ReactDOM.render(jsx, container);
