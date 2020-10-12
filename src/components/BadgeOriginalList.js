129704142735fbde8a5587773f87ef32274630f3
Carlos Eduardo Villanueva Altuna <carlos.villanueva.altuna@gmail.com>
2020-10-12 16:30:31 -0300

    Curso ReactJS

--- src/components/BadgeOriginalList.js
+++ src/components/BadgeOriginalList.js
@@ -26,14 +26,47 @@ class BadgesOriginalListItem extends React.Component {
   }
 }
 
-class BadgesOriginalList extends React.Component {
-  render() {
-    let badgesList=null;
+/**
+ * Para crear un buscador de badges vamos a utilizar Hooks, pero lo Hooks solo funcionan dentro de 
+ * componentes funcion y no dentro de clases, por lo que debemos convertir
+ * class BadgesOriginalList extends React.Component   en una function
+ * quitamos el metodo render(){}
+ */
+function BadgesOriginalList(props) {
+
+    const badges = props.badges;
+    //let badgesList=null;
+
+    /**
+     * Usamos useState
+     * query -> lo que se escribe en la caja
+     * setQuery() -> establecemos el valor
+     * Para hacer el filtrado haremos una combinación entre la lista de los badges y el query
+     */
+    const [ query, setQuery] = React.useState('');
+    //guardamos el resultado de los badges filtrados
+    let filteredBadgesList = badges.filter( badge=>{
+      //los badges que se deben retornar son los que se coincidan con nombre y apellido en el query
+      return `${badge.firstName} ${badge.lastName}`.toLowerCase().includes(query.toLowerCase());
+    });
+
+    //fin Hook
 
     /**validamos si vienen datos vacios */
-    if (this.props.badges.length === 0) {
+    if (filteredBadgesList.length === 0) {
       return (
         <div>
+
+          <div className="form-group">
+          <label> Filter Badges</label>
+          <input type="text" className="form-control" value={query} onChange={(event)=>{
+            //debemos guardar el valor y pasarlo a la propiedad value para ello usamos Hooks
+            //cuando sucedad el evento onChange leemos el valor del evento
+            //console.info(event.target.value);
+            setQuery(event.target.value)
+          }}/>
+        </div>
+
           <h3>No encontramos ningun Badge</h3>
           <Link className="btn btn-primary" to="/badges/new">
             Create new Badge
@@ -45,15 +78,25 @@ class BadgesOriginalList extends React.Component {
       //cuando almacenamos un nuevo badge este se va al final de lista con reverse() hacemos que el ultimo figure primero
       //si no queremos esas funcionalidad solo escribimos
       //this.props.badges.map(badge) en el return de abajo
-      badgesList = [...this.props.badges].reverse();
+      //filteredBadgesList = [...badges].reverse();
     }
 
     //al hacer click en algun elemento de la lista nos dirigimos a la información detallada del Badge
+    //cada vez que se escriba guardamos el valor que hay para luego utilizarlo pasandolo a traves del value
     return (
-      
       <div className="BadgesList">
+        <div className="form-group">
+          <label> Filter Badges</label>
+          <input type="text" className="form-control" value={query} onChange={(event)=>{
+            //debemos guardar el valor y pasarlo a la propiedad value para ello usamos Hooks
+            //cuando sucedad el evento onChange leemos el valor del evento
+            //console.info(event.target.value);
+            setQuery(event.target.value)
+          }}/>
+        </div>
+
         <ul className="list-unstyled">
-          {badgesList.map((badge) => {
+          {filteredBadgesList.map((badge) => {
             return (
               <li key={badge.id}>
                 <Link className="text-reset text-decoration-none" to={`/badges/${badge.id}`}>
@@ -66,7 +109,6 @@ class BadgesOriginalList extends React.Component {
         </ul>
       </div>
     );
-  }
 }
 
 export default BadgesOriginalList;
